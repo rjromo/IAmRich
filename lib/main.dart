757960e1aa4050185858f63951f2423cd562b207
Ruben <Romo>
2019-06-22 19:07:13 -0500

    Entrar

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.limeAccent,
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://cdn-images-1.medium.com/max/1200/1*SAtYQMezlgjpjbYeGmFDLQ.jpeg'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
